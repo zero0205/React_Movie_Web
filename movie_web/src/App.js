e6ff4a73cc7cc6897214a486d3d956cc137c2514
zero0205 <etety39@naver.com>
2022-02-15 16:29:38 +0900

    챕터 6까지(이론 끝!)

--- movie_web/src/App.js
+++ movie_web/src/App.js
@@ -1,17 +1,43 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("Created!");
+    return () => console.log("Destroyed");
+  }, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
   const onClick = () => setCounter((prev) => prev + 1);
+  const onClick2 = () => setShowing((prev) => !prev);
+  const onChange = (event) => setKeyword(event.target.value)
 
-  console.log("rendered");
-
+  useEffect(() => {
+    console.log("I run only once");
+  }, [])
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword])
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter])
   return (
     <div>
+      {showing ? <Hello /> : null}
+      <input
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
+      <button onClick={onClick2}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
